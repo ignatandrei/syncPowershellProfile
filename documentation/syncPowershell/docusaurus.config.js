a980dc613144164bb6bcf8b033b28f8951dee1e4
Andrei Ignat <ignatandrei@yahoo.com>
2023-08-19 08:57:20 +0300

    added link and debug actions

--- documentation/syncPowershell/docusaurus.config.js
+++ documentation/syncPowershell/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/syncPowershellProfile/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -22,7 +22,7 @@ const config = {
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
