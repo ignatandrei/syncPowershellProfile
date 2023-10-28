8b6503f95a9550e93d4be7338c44778cbd09aaa7
Andrei Ignat <ignatandrei@yahoo.com>
2023-10-28 18:37:51 +0300

    now with dlls

---
+++ src/SyncPowershell/syncPowershellWebAPI/Program.cs
@@ -0,0 +1,55 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#region export webapi
+//WebApiExportToFile.Extensions.AddExport(builder.Services);
+builder.Services.AddExport();
+#endregion
+
+#region links
+builder.Services.AddShortUrl();
+#endregion
+#region usefull endpoints
+builder.Services.AddSingleton<MiddlewareShutdown>();
+#endregion
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//WebApiExportToFile.Extensions.UseExport(app);
+app.UseExport();
+#region links
+app.UseShortUrl();
+#endregion
+//app.UseHttpsRedirection();
+#region usefull endpoints
+app.MapUsefullAll();
+app.MapHostedServices(app.Services.GetServices<IHostedService>().ToArray());
+//app.MapUsefullConfiguration();
+#endregion
+//app.UseAuthorization();
+#region blockly
+app.UseBlocklyUI(app.Environment);
+#endregion
+app.MapControllers();
+#region blockly
+app.UseBlocklyAutomation();
+#endregion
+
+#region ams
+app.UseAMS();
+#endregion
+
+#region links
+app.MapShortUrlEndpoints();
+#endregion
+app.Run();
---
+++ src/SyncPowershell/syncPowershellWebAPI/globals.cs
@@ -0,0 +1,8 @@
+﻿global using AMSWebAPI;
+global using NetCore2BlocklyNew;
+global using NetCoreShortLinks;
+global using UsefullExtensions;
+global using WebApiExportToFile;
+#region AMS
+[assembly: AMS_Base.VersionReleased(Name = "FirstRelease", ISODateTime = "2023-10-28", recordData = AMS_Base.RecordData.Merges)]
+#endregion
