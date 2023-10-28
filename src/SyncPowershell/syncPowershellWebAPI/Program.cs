3d72a3094e1b0438fb54f32134bd12c663818829
Andrei Ignat <ignatandrei@yahoo.com>
2023-10-28 21:52:04 +0300

    added nlog for logging

--- src/SyncPowershell/syncPowershellWebAPI/Program.cs
+++ src/SyncPowershell/syncPowershellWebAPI/Program.cs
@@ -1,55 +1,74 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-#region export webapi
-//WebApiExportToFile.Extensions.AddExport(builder.Services);
-builder.Services.AddExport();
-#endregion
-
-#region links
-builder.Services.AddShortUrl();
-#endregion
-#region usefull endpoints
-builder.Services.AddSingleton<MiddlewareShutdown>();
-#endregion
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Error("init main");
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    #region export webapi
+    //WebApiExportToFile.Extensions.AddExport(builder.Services);
+    builder.Services.AddExport();
+    #endregion
+
+    #region links
+    builder.Services.AddShortUrl();
+    #endregion
+    #region usefull endpoints
+    builder.Services.AddSingleton<MiddlewareShutdown>();
+    #endregion
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    //WebApiExportToFile.Extensions.UseExport(app);
+    app.UseExport();
+    #region links
+    app.UseShortUrl();
+    #endregion
+    //app.UseHttpsRedirection();
+    #region usefull endpoints
+    app.MapUsefullAll();
+    app.MapHostedServices(app.Services.GetServices<IHostedService>().ToArray());
+    //app.MapUsefullConfiguration();
+    #endregion
+    //app.UseAuthorization();
+    #region blockly
+    app.UseBlocklyUI(app.Environment);
+    #endregion
+    app.MapControllers();
+    #region blockly
+    app.UseBlocklyAutomation();
+    #endregion
+
+    #region ams
+    app.UseAMS();
+    #endregion
+
+    #region links
+    app.MapShortUrlEndpoints();
+    #endregion
+    app.Run();
 }
-//WebApiExportToFile.Extensions.UseExport(app);
-app.UseExport();
-#region links
-app.UseShortUrl();
-#endregion
-//app.UseHttpsRedirection();
-#region usefull endpoints
-app.MapUsefullAll();
-app.MapHostedServices(app.Services.GetServices<IHostedService>().ToArray());
-//app.MapUsefullConfiguration();
-#endregion
-//app.UseAuthorization();
-#region blockly
-app.UseBlocklyUI(app.Environment);
-#endregion
-app.MapControllers();
-#region blockly
-app.UseBlocklyAutomation();
-#endregion
-
-#region ams
-app.UseAMS();
-#endregion
-
-#region links
-app.MapShortUrlEndpoints();
-#endregion
-app.Run();
+catch(Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    NLog.LogManager.Flush();
+    NLog.LogManager.Shutdown();
+}
--- src/SyncPowershell/syncPowershellWebAPI/globals.cs
+++ src/SyncPowershell/syncPowershellWebAPI/globals.cs
@@ -3,6 +3,9 @@ global using NetCore2BlocklyNew;
 global using NetCoreShortLinks;
 global using UsefullExtensions;
 global using WebApiExportToFile;
+global using NLog;
+global using NLog.Web;
+
 #region AMS
 [assembly: AMS_Base.VersionReleased(Name = "FirstRelease", ISODateTime = "2023-10-28", recordData = AMS_Base.RecordData.Merges)]
 #endregion
