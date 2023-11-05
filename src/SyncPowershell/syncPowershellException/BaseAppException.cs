c2237914ef03c55991ac27e6faf95219b2d90fd4
Andrei Ignat <ignatandrei@yahoo.com>
2023-11-05 10:20:43 +0200

    added exception

---
+++ src/SyncPowershell/syncPowershellException/BaseAppException.cs
@@ -0,0 +1,13 @@
+﻿namespace syncPowershellException;
+public abstract class BaseAppException: Exception
+{
+    public BaseAppException(string message): base(message)
+    {
+        
+    }
+    public BaseAppException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+
+}
